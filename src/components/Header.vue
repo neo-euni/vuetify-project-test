a7d358b85e1ca403564b8e46e722d01e6d28e339
neo-euni <eh.park@neobiotech.com>
2024-11-11 18:04:48 +0900

    fix: 환자 정보 입력 시 오늘 날짜 이후는 생년월일 선택할 수 없음 함수 추가, 환자 정보 입력 후 save 버튼 시 성공하면 구강구조 나오도록 디자인함

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <v-app>
     <v-container>
       <v-stepper
+        v-model="activeStep"
         alt-labels
         editable
         :items="[
@@ -18,15 +19,12 @@
           <v-card>
             <v-card-title>
               <span>Patient List</span>
-              <v-btn
-                small
-                @click="showNewPatientForm.value = true"
-                class="new-patient-btn"
+              <v-btn small @click="createPatient" class="new-patient-btn"
                 >New Patient</v-btn
               >
             </v-card-title>
 
-            <v-card-text v-if="!showNewPatientForm.value">
+            <v-card-text>
               <v-list>
                 <v-list-item
                   v-for="(patient, index) in patientList"
@@ -45,10 +43,76 @@
           </v-card>
         </template>
 
-        <!-- 나머지 단계들 -->
+        <!-- 2단계 -->
         <template v-slot:item.2>
-          <v-card title="Create Case" flat>...</v-card>
+          <v-container>
+            <v-row justify="start">
+              <v-card>
+                <v-card-title>New Patient Case</v-card-title>
+                <v-card-text>
+                  <v-form ref="patientForm" @submit.prevent="saveNewPatient">
+                    <v-row>
+                      <!-- Name, ID, Gender, DOB 영역 -->
+                      <v-col cols="5">
+                        <v-text-field
+                          v-model="newPatient.name"
+                          label="Name"
+                          required
+                        ></v-text-field>
+                        <v-text-field
+                          v-model="newPatient.id"
+                          label="ID"
+                          required
+                        ></v-text-field>
+                        <v-select
+                          v-model="newPatient.gender"
+                          :items="['Male', 'Female']"
+                          label="Gender"
+                          required
+                        ></v-select>
+                        <v-text-field
+                          v-model="newPatient.dob"
+                          label="Date of Birth"
+                          type="date"
+                          :max="today"
+                          required
+                        ></v-text-field>
+                      </v-col>
+
+                      <!-- Memo 영역 -->
+                      <v-col cols="7">
+                        <v-textarea
+                          v-model="newPatient.memo"
+                          label="Memo"
+                          outlined
+                          :rows="11"
+                        ></v-textarea>
+                      </v-col>
+                    </v-row>
+
+                    <div class="new-patient-btns">
+                      <v-btn class="save-btn" @click="saveNewPatient"
+                        >Save</v-btn
+                      >
+                      <v-btn class="cancel-btn" @click="resetNewPatient"
+                        >Cancel</v-btn
+                      >
+                    </div>
+                  </v-form>
+                  <!-- 치아 이미지 들어와야 함  -->
+                  <v-img
+                    v-if="showMouthStructure"
+                    src="@/assets/mouth-structure.jpg"
+                    alt="Mouth Structure"
+                    max-width="400"
+                    class="mt-4"
+                  ></v-img>
+                </v-card-text>
+              </v-card>
+            </v-row>
+          </v-container>
         </template>
+
         <template v-slot:item.3>
           <v-card title="Simulation" flat>...</v-card>
         </template>
@@ -66,54 +130,94 @@
   </v-app>
 </template>
 
-<script>
+<script lang="ts">
 import { ref } from "vue";
+
+class Patient {
+  name: string;
+  id: string;
+  gender: string;
+  dob: string;
+  memo: string;
+
+  constructor(name = "", id = "", gender = "", dob = "", memo = "") {
+    this.name = name;
+    this.id = id;
+    this.gender = gender;
+    this.dob = dob;
+    this.memo = memo;
+  }
+
+  // 필요한 메서드를 추가할 수 있습니다.
+  reset() {
+    this.name = "";
+    this.id = "";
+    this.gender = "";
+    this.dob = "";
+    this.memo = "";
+  }
+}
 
 export default {
   setup() {
-    const showNewPatientForm = ref(false);
-    const patientList = ref([
-      { name: "John Doe", id: "001", gender: "Male", dob: "1990-01-01" },
-      { name: "Jane Smith", id: "002", gender: "Female", dob: "1985-05-15" },
-      { name: "Eunhee Park", id: "003", gender: "Male", dob: "1992-12-21" },
-      { name: "Sim Smith", id: "004", gender: "Male", dob: "1985-05-15" },
-      { name: "Jo Smith", id: "005", gender: "Female", dob: "1985-05-15" },
-    ]);
+    const STEP: number = 1; // 초기 단계 설정
+    const activeStep = ref(STEP);
+    const patientList = ref([]);
+    const newPatient = ref(new Patient());
+    const today = ref(new Date().toISOString().split("T")[0]);
+    const showMouthStructure = ref(false);
 
-    const newPatient = ref({
-      name: "",
-      id: "",
-      gender: "",
-      dob: "",
-    });
+    /**
+     *
+     * @param event
+     * @Description 클릭이벤트를 받으면 현재 단계를 1 증가 시킴 template에 v-model로 바인딩 되어 현재 단계를 화면에 반영시킴
+     * @return void
+     */
+    function createPatient(event: Event) {
+      activeStep.value++;
+    }
 
-    const addPatient = () => {
+    /**
+     * @Description 사용자가 입력한 환자 정보를 환자 리스트에 추가
+     * @return void
+     */
+    function saveNewPatient() {
       if (
         newPatient.value.name &&
         newPatient.value.id &&
         newPatient.value.gender &&
         newPatient.value.dob
       ) {
-        patientList.value.push({ ...newPatient.value });
-        resetNewPatientForm();
+        // 환자리스트에 저장한 환자 객체 추가
+        patientList.value.push(
+          new Patient(
+            newPatient.value.name,
+            newPatient.value.id,
+            newPatient.value.gender,
+            newPatient.value.dob
+          )
+        );
+        showMouthStructure.value = true;
       }
-    };
+    }
 
-    const cancelNewPatient = () => {
-      resetNewPatientForm();
-    };
-
-    const resetNewPatientForm = () => {
-      newPatient.value = { name: "", id: "", gender: "", dob: "" };
-      showNewPatientForm.value = false;
-    };
+    /**
+     * @Description 사용자가 입력한 환자 정보를 초기화
+     * @return void
+     */
+    function resetNewPatient() {
+      newPatient.value.reset();
+    }
 
     return {
-      showNewPatientForm,
+      activeStep,
+      createPatient,
       patientList,
       newPatient,
-      addPatient,
-      cancelNewPatient,
+      saveNewPatient,
+      resetNewPatient,
+      today,
+      showMouthStructure,
     };
   },
 };
@@ -124,6 +228,24 @@ export default {
   padding: 4px 8px;
   font-size: 10px;
   margin-left: 150px;
-  border: solid 1px rgb(12, 221, 203);
+  border: solid 1px #0cddcb;
+}
+
+.save-btn {
+  font-size: 10px;
+  padding: 4px 8px;
+  border: solid 1px #0cddcb;
+}
+
+.cancel-btn {
+  font-size: 10px;
+  padding: 4px 8px;
+  border: solid 1px rgb(250, 146, 139);
+}
+
+.new-patient-btns {
+  display: flex;
+  justify-content: flex-end;
+  gap: 8px;
 }
 </style>
